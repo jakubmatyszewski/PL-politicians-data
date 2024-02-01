84165f54b463cbd280aae15b73d4510599714166
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2024-02-01 15:25:03 +0100

    Switch to SejmAPI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PL-politicians-data
-Data of polish politicians scrapped from https://sejm.gov.pl/ 
+Data of polish politicians ~~scrapped~~ from https://sejm.gov.pl/ 
+
+---
+**1 Feb 2024**: Sejm API is available, scraping not required anymore.
--- app.py
+++ app.py
@@ -1,110 +1,37 @@
 import csv
 import logging
+import requests
 from datetime import date
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+
+URL = "https://api.sejm.gov.pl/sejm/term10/MP"
 
 
 class StanScrapper:
     def __init__(self):
-        """Start up the browser driver instance."""
-
         logging.basicConfig(level=logging.INFO)
-
-        options = Options()
-        options.headless = True
-
-        self.driver = webdriver.Firefox(
-            service=Service(GeckoDriverManager().install()), options=options
-        )
-        logging.info("Browser driver is opened.")
-
-        self.no_email = []
         self.output_data = []
-        self.running = False
 
-    def get_page(self, uid: int) -> None:
-        """
-        Direct `driver` to a page based on id (example id: `001`).
-        Grab element of interest as `body`.
-        """
-        url = "https://sejm.gov.pl/Sejm10.nsf/posel.xsp?id={:03d}".format(uid)
-        self.driver.get(url)
-        self.body = self.driver.find_element(By.ID, "title_content")
-        logging.info(f"Browser driver jumped to {uid} page.")
+    def get_data(self) -> None:
+        """Use API to get all"""
+        data = requests.get(URL).json()
+        for mp in data:
+            name = mp["firstLastName"]
+            district = f"{mp['districtNum']} {mp['districtName']}"
+            party = mp["club"]
+            age = self.get_age(mp["birthDate"])
+            email = mp["email"]
+            self.output_data.append((name, district, party, age, email))
 
-    def get_details(self) -> tuple:
-        """Get name, district, party and email of a politician"""
-
-        name = self.body.find_element(By.TAG_NAME, "h1").text
-        if not name:
-            # there are template blank pages at the end, catch it to finish
-            self.running = False
-            return ()
-
-        if self.check_if_rip():
-            name = "✝ " + name
-
-        district = self.body.find_element(By.ID, "okreg").text
-
-        try:
-            party = self.body.find_element(By.CSS_SELECTOR, "a[href*='klub']").text
-        except NoSuchElementException:
-            party = self.body.find_element(
-                By.XPATH, "//p[@id = 'lblKlub']/following-sibling::p[1]"
-            ).text
-
-        birth = self.body.find_element(By.ID, "urodzony").text
-        gender = self.get_gender()
-        age = self.get_age(birth)
-
-        try:
-            email_link = self.body.find_element(By.PARTIAL_LINK_TEXT, "adres email")
-        except NoSuchElementException:
-            email = None
-            self.no_email.append(name)
-        else:
-            # email is uncovered after a click
-            email_link.click()
-            email = email_link.text
-
-        return name, district, party, gender, age, email
-
-    def get_age(self, birth_info: str) -> int:
+    def get_age(self, birth_date: str) -> int:
         """Compute politician's age."""
-        try:
-            bdate = birth_info.split(",")[0]
-        except ValueError:
-            # There is no birth place given
-            bdate = birth_info
-        bday, bmonth, byear = [int(x) for x in bdate.split("-")]
+        bday, bmonth, byear = [int(x) for x in birth_date.split("-")]
         today = date.today()
         age = today.year - byear - ((today.month, today.day) < (bmonth, bday))
         return age
 
-    def get_gender(self) -> str:
-        """Get info on politician's gender. Return either `male` or `female`."""
-        helper = self.body.find_elements(By.XPATH, "//*[contains(text(), 'Wybrana dnia:')]")
-        if helper:
-            return "female"
-        return "male"
-
-    def check_if_rip(self) -> bool:
-        """Check if politician passed away."""
-        try:
-            self.driver.find_element(By.XPATH, '//*[contains(text(), "Zmarł")]')
-        except NoSuchElementException:
-            return False
-        else:
-            return True
-
-    def write_to_csv(self, data: list) -> None:
+    def write_to_csv(self) -> None:
         """Write gathered output to csv file."""
-        header = ["name", "district", "party", "gender", "age", "email"]
+        header = ["name", "district", "party", "age", "email"]
 
         try:
             # Create data.csv file if it doesn't exist
@@ -116,29 +43,41 @@ class StanScrapper:
             writer = csv.writer(f)
             writer.writerow(header)
 
-            for row in data:
+            for row in self.output_data:
                 writer.writerow(row)
         logging.info("Data saved successfully to `data/data.csv`.")
 
-    def tear_down(self) -> None:
-        """Close a browser driver instance."""
-        logging.info("Fetched all, closing browser.")
-        self.driver.quit()
 
-    def run(self):
-        """Loop to run the scrapper and save output."""
-        i = 0
-        self.running = True
+def drop_column(column: str) -> None:
+    """I had to do it."""
+    try:
+        f = open("data/data.csv", "r")
+    except FileExistsError:
+        return
+    else:
+        data = csv.DictReader(f)
+        headers = [f for f in data.fieldnames]
 
-        while self.running:
-            i += 1
-            self.get_page(i)
-            self.output_data.append(self.get_details())
+    if column not in headers:
+        logging.warn(f"{column} not found in csv file.")
+        logging.debug(f"Headers found: {headers}")
+        return
 
-        self.tear_down()
-        self.write_to_csv(self.output_data)
+    d = []
+    for row in data:
+        row.pop(column)
+        d.append(row)
+
+    headers.pop(headers.index(column))
+    f.close()
+
+    with open("data/data.csv", "w") as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writeheader()
+        writer.writerows(d)
 
 
 if __name__ == "__main__":
     s = StanScrapper()
-    s.run()
+    s.get_data()
+    s.write_to_csv()
